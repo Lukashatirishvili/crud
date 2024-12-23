1b0a0aaa0ac141af6b8663e5b1dd470d478c37b4
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2024-12-23 17:06:04 +0400

    first commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/CrudContext.cs
@@ -0,0 +1,15 @@
+﻿using crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class CrudContext : DbContext
+    {
+        public CrudContext(DbContextOptions<CrudContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
